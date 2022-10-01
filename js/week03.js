3278297542bbe239592a3de95a8e2a0bfd6212c6
Christian <vargasc475@gmail.com>
2022-10-01 15:35:21 -0400

    added week03 page

--- js/week03.js
+++ js/week03.js
@@ -1,29 +1,3 @@
-/*
-let ladder = {
-    step: 0,
-    up() {
-      this.step++;
-      return this;
-    },
-    down() {
-      this.step--;
-      return this;
-    },
-    showStep: function() { // shows the current step
-      alert( this.step );
-      return this;
-    }
-  };
-
-// ladder.up();
-// ladder.up();
-// ladder.down();
-// ladder.showStep(); // 1
-// ladder.down();
-// ladder.showStep(); // 0
-
-ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0 */
-
 const randomFoods = {
   latinFood: [
     {
@@ -124,11 +98,6 @@ const randomFoods = {
   },
 
 }
-
-
-
-
-
 
 
 const buttonL = document.getElementById('latin');
